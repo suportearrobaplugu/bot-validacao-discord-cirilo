3914b0098eb0772585002f4a1f17d3f19f450425
suportearrobaplugu <suporte@plugu.com.br>
2025-07-09 18:27:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,54 @@ const express = require('express');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { google } = require('googleapis');
 const cron = require('node-cron');
-const fs = require('fs');
 
-// 🌐 Express para manter o Replit online
+// 🌐 Express para manter o Railway online
 const app = express();
 app.get('/', (req, res) => res.send('Bot rodando!'));
 app.listen(3000, () => console.log('Servidor express ativo na porta 3000'));
 
-// 📋 Carregar credenciais da conta de serviço
+// 📋 Credenciais direto no código (⚠️ NÃO USE EM PRODUÇÃO PÚBLICA)
 const { GoogleAuth } = require('google-auth-library');
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
-console.log('[DEBUG] Primeiros caracteres da variável GOOGLE_CREDENTIALS_JSON:', process.env.GOOGLE_CREDENTIALS_JSON?.slice(0, 100));
+const credentials = {
+  "type": "service_account",
+  "project_id": "bot-discord-validacao",
+  "private_key_id": "c61dd1da64e2784b2728e639ce4f8f3814782789",
+  "private_key": `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCiAK5E2IWNGAmx
+JVx7RcXoHgjn2RxrNyfB+gi770xu/pVUjHjviFMWcXGL7MXgJWOoOCaMkM0xUEtG
+nLRAwcyAsc/hclAcOSV1gvoKUmpx5pAmqWAAXZsBJbD1sjoIWJYdz67yKXxU7d5E
+p8oWDETgg/FX8SBgzAFoVRPjYoMwFvm+6+jQ/TCUcNn4zowu2DI4huzghTNOrc3r
+Un3LjAnBl7e59MPYxKk4WwLDCAQLYWg0CBoQIIYExGr8Jrl1GaHVXlSOJSwgYWSO
+fykMfWDU/ejFnsWvpVCVbTiPgIGW770FCr+k/SeIjJ/v9j8GiGantVbxm7AM3C45
+JSZzmvIzAgMBAAECggEAQ8YoIfo5aQ0w5NDeD15hzNlf3Wr5lislcWPiqokSv263
+V39LCt7L79w25hSAteS+e/49tlnP1j0iuOBPErZhEUgSUV1rAVuw/af1ZkMTcCJG
+lT4PwSdn9QNpNGjDGYHCD6e2p23Oq5M2BR/zDeVFHW5lleZ1JrnCLv1EzECG5KNU
+gns0guG8GOJ/3WHBdBX7TuFDaJJsOaWu7r8uoGQxafb04L+tRwGPSU9fJAp8faSx
+CU02Ifs3jL4788VEYv8iGo05Qdo2n4g7rl3jbr+GetMvshjH6YQwTc+29ij1OSbx
++Fin/kxBXm8p2v0XhtE2yOLiOtYPhqDY7BBuj3uqHQKBgQDkgKsB3D/aagiR8kXg
+5ZbwNfGuv09yXt6M7kB8ugOq7S47WxX+KQFybTsjMHJMp1FFnGsC3pnh4p3afHxn
+YlQhJxqBsWUVnu/lAWMTzj48WkF4Zx4NwdMWSJ/12/9HfFJ4MkAc8oD254C3HIPB
+YTxO2SLgEqdR6DgULnStKO1RPwKBgQC1f2RNyhSr3qsljKQ71wjlOoVZJyBpMzSl
+C6wn/7SHA+DgS1m5EghL6RhaThFnmEPbODDYn61ZCkXkrWMWHNdkQI9tkDlGPnNz
+zxItncsqCoQ+T3AHP+ODK+I4RKVoydeXh1sdW/Ed7zdoiGMCfq0+nZtWkmM6e6QC
+BKnio2uuDQKBgBL89jjzWoMF6KxoG5JywbY4ZVsGHs38QDYp3kX/FyrvBrZ7/Tnf
+onUlypJEBluEU+aa9vGIGfWBZWiwi43zx9Xxb+xzF1lSmdDH6rnMm7NcNLCSsRan
+kqj5ZAGERJRmKlw+oMFHDZx25GVTA0fgkqhittkz85+E0pQqwBtbmUz1AoGBAI5G
+VKyeEuI/5M3EmCHIiVKYM1wVzdRprYULx2J7klqccbWhm632Msa+7LYtqHsa42Z/
+f9rwIQVBhlSijqAQ78/IxCZQ/nTQE9TeqBNWNKK6J7xorRshvAlhZy5QsZGZWAWS
+t5wmLLcYhMjO0T5tawVAqzqL+nkn3wMuALaHAKwpAoGABQ0Rv1OEiiZdvWXp/doq
+LUC5cHvZrxacYXJnk17pPQ1ylCmOP/bmO3xPzK/SV/SS85ENnqlDlnFcRZCvFSwZ
+xf/dRe3PAdFgp4NelusswXMUKMCVFfypP+ifY9DzB61phIdG5PwsEbS21aHSNrre
+1HhJozuwd7h9m+zp5zwyylI=
+-----END PRIVATE KEY-----`,
+  "client_email": "bot-discord@bot-discord-validacao.iam.gserviceaccount.com",
+  "client_id": "118146359298173031900",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bot-discord%40bot-discord-validacao.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
 
 const auth = new GoogleAuth({
   credentials,
@@ -81,5 +118,5 @@ client.on('messageCreate', async (message) => {
   }
 });
 
-// 🛡️ Token do bot (substitua pela sua chave real)
+// 🛡️ Token do bot
 client.login(process.env.DISCORD_TOKEN);
